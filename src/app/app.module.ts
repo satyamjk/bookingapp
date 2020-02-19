3746f45cf43975cdfb8cacdfcee91030effd40fa
Satyam Jha <satyamkumarindev@gmail.com>
2020-02-19 22:04:48 +0530

    Adds rental list and detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
+import { Routes,RouterModule, Route} from '@angular/router'
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
+import { RentalComponent } from './rental/rental.component';
+import { TempComponent } from './temp/temp.component';
+
+import { RentalModule } from './rental/rental.module';
+
+
+const routes: Routes = [
+  {path: '', redirectTo: '/rentals', pathMatch: 'full'},
+  {path: 'temp', component: TempComponent}
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TempComponent
+    
+    
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    RentalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/rental/rental-detail/rental-detail.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RentalService } from '../shared/rental.service';
+import { Rental } from '../shared/rental.model';
+
+@Component({
+  selector: 'bwm-rental-detail',
+  templateUrl: './rental-detail.component.html',
+  styleUrls: ['./rental-detail.component.scss']
+})
+export class RentalDetailComponent implements OnInit {
+
+  currentId: string; 
+
+  rental: Rental;
+
+  constructor(private route: ActivatedRoute,
+              private rentalService: RentalService) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params) => {
+        this.currentId = params['rentalId'];
+
+        this.getRental(params['rentalId']);
+      }
+    )
+    }  
+
+    getRental(rentalId: string){
+      this.rentalService.getRentalsById(rentalId).subscribe
+      ((rental: Rental) => { 
+        this.rental = rental;
+      })
+    }
+    
+  
+
+}
